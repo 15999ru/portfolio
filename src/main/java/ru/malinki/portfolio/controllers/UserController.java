5a0729472b9c5b522fe1b8cc5dce5654fe00b776
15999ru <hudakovv93@gmail.com>
2025-04-23 22:25:48 +0500

    Update UserController.java

--- src/main/java/ru/malinki/portfolio/controllers/UserController.java
+++ src/main/java/ru/malinki/portfolio/controllers/UserController.java
@@ -1,6 +1,5 @@
 package ru.malinki.portfolio.controllers;
 
-import jakarta.servlet.http.Cookie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
